70647c78b7b334426ff2bc543bb504ecdd766c11
John Evans <lgastako@gmail.com>
2013-12-31 17:01:30 -0500

    Remove extraneous debugging output.

--- src/launchtone/cron.clj
+++ src/launchtone/cron.clj
@@ -2,7 +2,6 @@
   (:require [overtone.at-at :as at-at]))
 
 (defn every [app ms f]
-  (println "every - worker pool" (@app :worker-pool))
   (at-at/every ms f (@app :worker-pool)))
 
 (defn after [app ms f]
