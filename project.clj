415218c2a78c2037cd94bbbab50cad71f3fd4888
John Evans <lgastako@gmail.com>
2014-01-01 17:56:53 -0500

    Bump version in preperation for release.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject launchtone "0.1.0-SNAPSHOT"
-  :description "Interface with the Launchpad from Overtone"
+(defproject launchtone "0.1.0"
+  :description "Interface with the Launchpad S from Overtone"
   :url "http://github.com/lgastako/launchtone"
   :license {:name "Public Domain"
             :url "http://creativecommons.org/publicdomain/zero/1.0/"}
   :main ^:skip-aot launchtone.demos
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [overtone "0.9.1"]
-                 [subwatch "0.1.0-SNAPSHOT"]
+                 [subwatch "1.0.0"]
                  [org.clojure/math.combinatorics "0.0.7"]])
